e4b03d51c677b23c8cf0c94060a4ed9a95f10849
Roy Chen <roychenlei@gmail.com>
2018-02-15 21:27:28 +0800

    Write some for loops

---
+++ 05_for.go
@@ -0,0 +1,28 @@
+package gobyexample
+
+import "fmt"
+
+// DoSomeLooping - run some loops
+func DoSomeLooping() {
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i++
+	}
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("inside loop")
+		break
+	}
+
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
