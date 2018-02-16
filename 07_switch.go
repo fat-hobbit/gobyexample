097175fb36c1569722c0a723972d779ca96addda
Roy Chen <roychenlei@gmail.com>
2018-02-16 22:39:53 +0800

    Write some switch statements

---
+++ 07_switch.go
@@ -0,0 +1,54 @@
+package gobyexample
+
+import "fmt"
+import "time"
+
+// RunSwitchStatements - run some switch statements
+func RunSwitchStatements() {
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	// switch with multiple expressions in same case,
+	// and default case
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// switch without an expression, an alternative
+	// way to express if/else logic
+	// case expressions can also be non-constants
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	// type switch compares types instead of values
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
